4831cde2539bf2005a81d7f3d45b26ed8fa92407
axelgolin4 <axelgolin4@gmail.com>
2022-04-13 13:10:38 -0600

    2.interfaz

--- src/barberodormilon/BarberoDormilon.java
+++ src/barberodormilon/BarberoDormilon.java
@@ -14,7 +14,7 @@ public class BarberoDormilon {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+            new barberia().setVisible(true);
     }
     
 }
